8ed413c0ad19dfe800d1c364fd603aa9923860e7
fvntr <phiven@gmail.com>
2015-07-02 11:42:55 -0700

    first attempt at pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //this.stack = stackMethods; 
+  this.storage = {};
+  this.indexOfLastItem = 0;
+
 };
+	
+var stackMethods ={};
 
 
+stackMethods.size = function(){
+  //this refers to the specific instance of the stack that is calling this method
+  this.indexOfLastItem;
+}
+stackMethods.push = function(value){
+  //this refers to the specific instance of the stack that is calling this method
+  this.indexOfLastItem++;
+  this.storage[this.indexOfLastItem] = value;
+}
+stackMethods.pop = function(){
+  //this refers to the specific instance of the stack that is calling this method
+  if(this.indexOfLastItem > 0){
+    this.indexOfLastItem--;
+  }
+    this.storage[this.indexOfLastItem+1];
+}
+
+var stack = new Stack();
+
+
+
+console.log(stack);
