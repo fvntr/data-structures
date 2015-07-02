18bd5a12de74ee16b72263988c97a3de21533068
fvntr <phiven@gmail.com>
2015-07-01 19:10:44 -0700

    copmleted queue for functional

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,35 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+	var queue = {};
+
+	queue.storage = {};
+
+	queue.indexOfLastItem = 0;
+	queue.indexOfFirstItem = 0;
+
+	extend(queue, queueMethods);
+
+	return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.size = function(){
+	return this.indexOfLastItem - this.indexOfFirstItem;
+};
 
+queueMethods.enqueue = function(value){
+	this.indexOfLastItem++;
+	return this.storage[this.indexOfLastItem] = value;
+}
 
+queueMethods.dequeue = function(){
+
+	if(this.indexOfLastItem - this.indexOfFirstItem){
+		this.indexOfFirstItem++;
+	}
+	
+	return this.storage[this.indexOfFirstItem];
+}
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -7,7 +7,7 @@ var Stack = function() {
 
   //this represnts the "place" where items are stored on the stack 
   stack.storage = {};
-  
+// 
   // cannot impliment the next line, we think the compiler will choke
   // var size = 0;
 
@@ -16,6 +16,7 @@ var Stack = function() {
 
   //this sends the stack into the specified stackMethod(either push or pop)
   extend(stack, stackMethods)
+
 
   //this return the stack, which is an objects. All properties are functions.
   return stack;
@@ -26,16 +27,18 @@ var Stack = function() {
 var stackMethods = {};
 
 stackMethods.size = function(){
+  //this refers to the specific instance of the stack that is calling this method
   return this.indexOfLastItem;
 }
 
 stackMethods.push = function(value){
+  //this refers to the specific instance of the stack that is calling this method
   this.indexOfLastItem++;
-  console.log('this.indexOfLastItem in push:', this.indexOfLastItem);
   return this.storage[this.indexOfLastItem] = value;
 }
 
 stackMethods.pop = function(){
+  //this refers to the specific instance of the stack that is calling this method
   if(this.indexOfLastItem > 0){
     this.indexOfLastItem--;
     return this.storage[this.indexOfLastItem+1];
