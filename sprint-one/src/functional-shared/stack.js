44d4c4a3e0e0f0bad46e677ea69f3806889f3eb3
fvntr <phiven@gmail.com>
2015-07-01 17:55:23 -0700

    making progress!!!

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,67 +1,49 @@
 
 
-var Stack = function() {   // Hey! Rewrite in the new style. Your code will
-// wind up looking very similar,   // but try not not reference your old code in
-// writing the new style.   // var size = 0;
-
-  // console.log( 'stackMethods.size :', stackMethods.size);
-
-  //implement this somewhere here 
-  //is it like this.method
-  //
+var Stack = function() {  
+  
+  //this represents the initial state when a new stack is created
   var stack = {};
 
-  //stack.num = 0;
-  testAB = 0;
+  //this represnts the "place" where items are stored on the stack 
+  stack.storage = {};
+  
+  // cannot impliment the next line, we think the compiler will choke
+  // var size = 0;
+
+
+  stack.result = 0;
+  stack.add = stackMethods.add;
 
   stack.size = stackMethods.size;
-  stack.add = stackMethods.add;
-  //stack.add = stackMethods.add;
-  // stack.add = extend(stack, stackMethods)
-  // stack.size = 0;
-  // var size = function(){ return 0;}
-
-	//  console.log('Stack.size :', stack.size);
-	//testAB++;
-  //console.log('testAB++ :', testAB++);
   
-  return stack;
+  //need to make these
+  stack.push = stackMethods.push;
+  stack.pop = stackMethods.pop;
 
+  return stack;
 }  
 
 
 
-var stackMethods = {
+var stackMethods = {};
 
-	size : function( ){return testAB;},
-  add : function( ){
-    stack.push();
-    return testAB + 2;},
-	//size : function(){return this.size;},
-	//console.log('this.size; is: ', this.size)
-	//shift
-	// add: function(){
- //    testAB++; 
- //    console.log('testAB++ :', testAB++); 
- //    return testAB;}
-	// //pop
-
-	//size: 0
+stackMethods.size = function(){
+  return this.result;
 }
 
+stackMethods.push = function(value){
+  this.result++;
+  console.log(this.storage)
+  return this.storage[this.result] = value;
+}
 
-// var extend = function(copyTo, copyFrom) {
-//     for (var property in copyFrom) {
-//         copyTo[property] = copyFrom[property];
-//     }
-// };
+stackMethods.pop = function(){
+  return this.result;
+}
 
-
-  
-// var testA = new Stack();
-// console.log('Stack.size :', stack.size);
-// console.log('stackMethods.size :', stackMethods.size);
-//console.log('testA is :', testA);
-
-// at Context.<anonymous> (file:///Users/student/Desktop/2015-06-data-structures/sprint-one/spec/specs.js:42:15)
-//     at Test.require.register.Runnable.run (
+var extend = function(copyTo, copyFrom) {
+    for (var property in copyFrom) {
+        copyTo[property] = copyFrom[property];
+    }
+};
