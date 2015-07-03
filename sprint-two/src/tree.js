5916b9331c382290f2926a9135e9526554ea4a45
fvntr <phiven@gmail.com>
2015-07-03 14:27:34 -0700

    implement tree data structure addChild

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,23 @@
+/*
+A tree class, in functional with shared methods style, with the following properties:
+.children property, an array containing a number of subtrees
+.addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+What is the time complexity of the above functions?
+
+
+*/
+
+
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  
+
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -15,7 +29,8 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	
+	this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
