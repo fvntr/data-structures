5f3f737cdfbdad7c8e86fb198fb8e94d2ca6b7da
fvntr <phiven@gmail.com>
2015-07-03 12:26:54 -0700

    implement all methods in linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,4 +1,3 @@
-
 /*
 A linkedList class, in functional style, with the following properties:
  .head property, a linkedListNode instance
@@ -7,40 +6,24 @@ A linkedList class, in functional style, with the following properties:
  .removeHead() method, removes the first node from the list and returns its value
  .contains() method, returns boolean reflecting whether or not the passed-in value is in the linked list
  */
-
-/*
-
-linkedList.addToTail(4);
-linkedList.addToTail(5);
-expect(linkedList.head.value).to.equal(4);
-linkedList.removeHead();
-expect(linkedList.head.value).to.equal(5)
-
-
-*/
-
-
 var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
-    
-    var pod = Node(value); 
+    //instantiates a new node
+    var localNode = Node(value); 
 
     if(list.tail === null){
-      list.head = Node(value);
+      list.head = localNode;
 
     } else {
-      //update the current tail node to point to (ie the node.next value on that node should be) the new value that is being passed in and will become the new tail node
-      
-      console.log("next before :", list.tail.next);
-      list.tail.next = pod;
-      console.log("next after :", list.tail.next);
+      //update the current tail node to point to (ie the node.next value on that node should be) the new value that is being passed in and will become the new tail node     
+      list.tail.next = localNode;
     };
     // console.log("next before :", list.tail);
-    list.tail = pod;
+    list.tail = localNode;
     // console.log("next after :", list.tail);
   };
 
@@ -50,21 +33,34 @@ var LinkedList = function(){
     //return value
     //should remove the head from the list when removeHead is called
 
-
     var temp = list.head.value; 
-
-    // console.log("before", list.head.next);
-    // console.log("before list", list);
-
-    //console.log("before", "head:", list.head, "tail:", list.tail);
+  
     list.head = list.head.next;
-    // console.log("after", "head:", list.head, "tail:", list.tail);
-    //console.log("after", list.head);
-    return temp.value; 
+
+    return temp; 
 
   };
 
   list.contains = function(target){
+    //we are creating a fxn that traverses each node of the linkedlist, starting at the head
+    var traverseLinkedList = function(node){
+      //check if the node.value is the same as the target
+      if(target === node.value){ 
+        //if yes, return true (and we want to exit the fxn)
+        return true;
+
+      //if not, check if the current's node next key is null
+      } else if(node.next === null){        
+        //if yes (if the current's node next key is null), we are at the end of the list. the target is not in the list (return false and we want to exit the fxn)
+        return false;
+      } 
+
+      //if there is a next node value and we have to run the function on the node's next value
+      return traverseLinkedList(node.next);
+
+    };
+
+    return traverseLinkedList(list.head);
   };
 
   return list;
