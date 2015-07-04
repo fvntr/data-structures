412f3e2ae23c9b8818fb66c609699888e1e8a581
fvntr <phiven@gmail.com>
2015-07-04 10:43:05 -0700

    completed hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -19,16 +19,34 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, [v]);
+
+ 	if(this._storage.get(i) === undefined){
+		this._storage.set(i, [v]);
+  } else {
+			this._storage.get(i).push(k,v);	
+		} 
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  console.log('this._storage', this._storage);
+  console.log('this._storage.get(i)', this._storage.get(i));
+  console.log('length', this._storage.get(i).length);
+
+  if (this._storage.get(i).length > 1){
+  	for(var j = 1; j < this._storage.get(i).length; j+2){
+  		if(this._storage.get(i)[j] === k){
+  			return this._storage.get(i)[j+1]
+  		}
+  	}
+  }
   return this._storage.get(i)[0];
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  // return this._storage.set(i, null);
   return this._storage.set(i, [null]);
 };
 
