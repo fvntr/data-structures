1dc24762ab5b320a50cf1a40383b22305efef22c
fvntr <phiven@gmail.com>
2015-07-03 19:38:49 -0700

    pass all graph tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,29 +1,88 @@
+/*
+Implement a graph class, in pseudoclassical style, with the following properties:
+It is an undirected graph. It does not have to be 'connected'.
+An .addNode() method that takes a new node and adds it to the graph
+A .contains() method that takes any node and returns a boolean reflecting whether it can be found in the graph
+A .removeNode() method that takes any node and removes it from the graph, if present. All edges connected to that Node are removed as well.
+An .addEdge() method that creates a edge (connection) between two nodes if they both are present within the graph
+A .hasEdge() method that returns a boolean reflecting whether or not two nodes are connected
+A .removeEdge() method that removes the connection between two nodes
+A .forEachNode() method that traverses through the graph, calling a passed in function once on each node
 
+*/
 
 var Graph = function(){
+	//stores the values
+	this.storage = [];
+	//tracks the edges associated with the values
+	this.edge = [];
 };
 
 Graph.prototype.addNode = function(node){
+	//when we add a new node to the storage arry, also add it AS AN ARRAY to the edge array
+	this.edge.push([node]);
+	//add the new node to the storage array and return it
+	return this.storage.push(node);
 };
 
 Graph.prototype.contains = function(node){
+	//search for the target value by looping through the storage array 
+	//if the value is in the storage array return true
+	//else return false
+	for (var i = 0; i < this.storage.length; i++){
+		if(this.storage[i] === node){
+			return true;
+		}
+	}
+	return false;
 };
 
 Graph.prototype.removeNode = function(node){
+	//search for the target node to delete by looping through the storage array 
+	//if the value is in the storage array, 
+		//delete if from the edage array and from the storage array
+		//return the deleted value
+	for (var i = 0; i < this.storage.length; i++){
+		if(this.storage[i] === node){
+			var temp = this.storage[i];
+			this.storage.splice(i,1);
+			this.edge.splice(i,1);
+			return temp;
+		}
+	}
+
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+	var result = false;
+		
+	for(var i=0; i<this.edge[this.storage.indexOf(fromNode)].length; i++){
+		if(this.edge[this.storage.indexOf(fromNode)][i] === toNode){
+			result = true;
+		}
+	}
+
+	return result;
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	this.edge[this.storage.indexOf(fromNode)].push(toNode); 
+	this.edge[this.storage.indexOf(toNode)].push(fromNode); 
+
+
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+
 };
 
 Graph.prototype.forEachNode = function(cb){
-};
-
+	for(var i = 0; i<this.storage.length; i++){
+		cb(this.storage[i]);
+	}	
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,7 +2,8 @@
 A tree class, in functional with shared methods style, with the following properties:
 .children property, an array containing a number of subtrees
 .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
-A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+A .contains() method, takes any input and returns a boolean 
+reflecting whether it can be found as the value of the target node or any descendant node
 What is the time complexity of the above functions?
 
 
@@ -29,12 +30,44 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-	
+	//this represents an instantiation of a tree
+	//addChild pushes a new instantiation of tree with the specific value being passed in 'this'
 	this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
+	// console.log(this);
 
+	//check if current tree value === target
+	//if yes, return true
+	//else check the children nodes, and repeat process
+		//check if current tree value === target
+		//if yes, return true
+		//else if the current tree value is null, 
+			//return false
+		//else check if there are children node
+			//if yes, check each child's value
+			//if no, return false
+	var traverseTree = function(treeNode){
+		if(treeNode.value === target){
+			return true;
+		} else if (treeNode.value === null){
+			return false;
+		} else {
+			if(treeNode.children.length > 0){
+				// console.log('treeNode.children.length', treeNode.children.length);
+				for(var i = 0; i < treeNode.children.length; i++){
+					// console.log('target', target);
+					// console.log('treeNode.children', treeNode.children[i]);
+					traverseTree(treeNode.children[i]);
+				}
+			} else {
+				return false;
+			}
+		}		
+	}(this.children[0]);
+
+	return traverseTree;
 };
 
 
