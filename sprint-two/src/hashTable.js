9470fe328388cbe9567c346bc4c1e7401b8757df
fvntr <phiven@gmail.com>
2015-07-03 21:14:37 -0700

    completed through removed, collisions are still out standing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -15,37 +15,21 @@ A .remove() method
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  // this.values = [];
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  console.log('i :', i);
-  console.log('k :', k);
-  console.log('v :', v);
-  console.log('this._storage', this._storage);
-  // console.log('this.values', this.values);
-  // console.log('this.values[i]', this.values[i]);
-  // console.log('this._storage[i]', this._storage[i]);
-
-  this._storage.set(i, v);
-  // this.values[i].push(k,v);
-
+  this._storage.set(i, [v]);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  return this._storage.get(i)[0];
 };
 
 HashTable.prototype.remove = function(k){
-	// var i = getIndexBelowMaxForKey(k, this._limit);
-  // console.log('i :', i);
-  console.log('k :', k);
-  console.log('this._storage :', this._storage);
-  console.log('this._storage', this._storage);
-	
-	return this._storage.each[k] = null;
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.set(i, [null]);
 };
 
 
